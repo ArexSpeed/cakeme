90b5f76d92073b5ccf3c1c573657d8adf346c640
Arek Cichocki <arexspeed95@gmail.com>
2021-05-06 19:32:45 +0200

    back to refresh on like

--- components/CardItem/index.js
+++ components/CardItem/index.js
@@ -1,45 +1,10 @@
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Favorite, AddShoppingCart, Visibility } from '@material-ui/icons';
 import { useSession } from 'next-auth/client';
-import useSWR from 'swr';
-import { jsonFetcher } from 'utils';
 //style Card
 
 const CardItem = ({ item }) => {
-  const [favor, setFavor] = useState([])
-  const { data } = useSWR(`/api/favorites/get`, jsonFetcher, { initialData: favor });
   const [session, loading] = useSession();
-  const [like, setLike] = useState(false);
-
-  useEffect(async () => {
-    const response = await fetch(`/api/favorites/get`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    
-    //console.log(favs, 'favs');
-    console.log(data, 'data in useefeckt')
-  }, []);
-
-  const addFav = async () => {
-    const payload = {
-      product: item,
-      user: session.user
-    };
-    const response = await fetch(`/api/favorites`, {
-      method: 'POST',
-      body: JSON.stringify(payload),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    if (response.ok) {
-      setLike(true);
-    }
-  };
 
   const toggleFavorite = async () => {
     console.log('toggle');
@@ -48,7 +13,7 @@ const CardItem = ({ item }) => {
       user: session.user
     };
     if (item.favoriteEmail?.includes(session.user.email)) {
-      await fetch(`/api/favorites`, {
+      await fetch(`/api/products/favorite`, {
         method: 'DELETE',
         body: JSON.stringify(payload),
         headers: {
@@ -56,7 +21,7 @@ const CardItem = ({ item }) => {
         }
       });
     } else {
-      await fetch(`/api/favorites`, {
+      await fetch(`/api/products/favorite`, {
         method: 'PUT',
         body: JSON.stringify(payload),
         headers: {
@@ -87,7 +52,7 @@ const CardItem = ({ item }) => {
       <div className="card__actions">
         {session && !loading && (
           <button
-            onClick={addFav}
+            onClick={toggleFavorite}
             className={item.favoriteEmail?.includes(session.user.email) ? 'favorite' : ''}>
             <Favorite />
           </button>
