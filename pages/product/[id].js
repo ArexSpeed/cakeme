f7a5b3e9a2250970de210512753f9751395a5bca
Arek Cichocki <arexspeed95@gmail.com>
2021-09-17 22:16:26 +0200

    new product site

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -41,7 +41,7 @@ export default function ProductPage({ product, bakeryProducts }) {
 
   useEffect(() => {
     if (product) {
-      setIngredients(product.ingredients?.split(','));
+      setIngredients(product.ingredients);
     }
   }, [product]);
 
@@ -102,49 +102,114 @@ export default function ProductPage({ product, bakeryProducts }) {
     <Layout>
       <main className="main">
         <section className="product">
-          <div className="product__price">From {product?.price}</div>
-          <article className="product__top">
-            <img
-              src={
-                product?.imageUrl
-                  ? product?.imageUrl
-                  : 'https://www.przyslijprzepis.pl/media/cache/big/uploads/media/recipe/0007/27/domowy-drip-cake_1.jpeg'
-              }
-              alt=""
-            />
+          <div className="product__title">
+            <div className="product__title-name">{product?.name}</div>
+            {session && !loading && (
+              <button className="product__title-button" onClick={toggleFavorite}>
+                <Favorite
+                  style={{
+                    color: like ? 'orange' : '',
+                    width: '32px',
+                    height: '32px'
+                  }}
+                />
+              </button>
+            )}
+          </div>
+          <article className="product__container">
+            <div className="product__images">
+              <img
+                src={
+                  product?.imageUrl
+                    ? product?.imageUrl
+                    : 'https://www.przyslijprzepis.pl/media/cache/big/uploads/media/recipe/0007/27/domowy-drip-cake_1.jpeg'
+                }
+                alt=""
+              />
+              <div className="product__images-small">
+                <button>
+                  <img
+                    src={
+                      product?.imageUrl
+                        ? product?.imageUrl
+                        : 'https://www.przyslijprzepis.pl/media/cache/big/uploads/media/recipe/0007/27/domowy-drip-cake_1.jpeg'
+                    }
+                    alt=""
+                  />
+                </button>
+                <button>
+                  <img
+                    src={
+                      product?.imageUrl
+                        ? product?.imageUrl
+                        : 'https://www.przyslijprzepis.pl/media/cache/big/uploads/media/recipe/0007/27/domowy-drip-cake_1.jpeg'
+                    }
+                    alt=""
+                  />
+                </button>
+                <button>
+                  <img
+                    src={
+                      product?.imageUrl
+                        ? product?.imageUrl
+                        : 'https://www.przyslijprzepis.pl/media/cache/big/uploads/media/recipe/0007/27/domowy-drip-cake_1.jpeg'
+                    }
+                    alt=""
+                  />
+                </button>
+              </div>
+            </div>
             <div className="product__info">
-              <h2>{product?.name}</h2>
-              <p>{product?.description}</p>
-              <p>Ingredients:</p>
-              <ul>
-                {ingredients?.map((item, i) => (
-                  <li key={i}>{item}</li>
-                ))}
-              </ul>
+              <div className="product__price">{product?.price},00 $</div>
+              <div className="product__rank">
+                <h4 className="product__rank-score">5,00</h4>
+                <svg
+                  className="product__rank-star"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="product__rank-star"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="product__rank-star"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+              </div>
+              <div className="product__description">{product?.description}</div>
+              <div>
+                <p className="product__ingredients">Ingredients:</p>
+                <p className="product__description">{ingredients}</p>
+              </div>
+              {session && !loading && (
+                <button className="product__add">
+                  <AddToCartButton item={product} /> <span>Add to cart</span>
+                </button>
+              )}
             </div>
           </article>
           <article>
-            {session && !loading && (
-              <div className="productActions">
-                <button onClick={toggleFavorite}>
-                  <Favorite
-                    style={{
-                      color: like ? 'orange' : ''
-                    }}
-                  />
-                  <span>Like it</span>
-                </button>
-                <AddToCartButton item={product} />
-              </div>
-            )}
-            {!session && (
-              <div className="productActions">
-                <h4>You have to login to order this product</h4>
-              </div>
-            )}
-
-            <p>Other products from {product?.bakery[0]}</p>
-            <section className="section">
+            <div className="product__sectionName">
+              <span>Other products from {product?.bakery[0]}</span>
+            </div>
+            <section className="product__other">
+              {bakeryProducts?.map((item) => (
+                <CardItem key={item.id} product={item} />
+              ))}
+            </section>
+            <div className="product__sectionName">
+              <span>Similar products</span>
+            </div>
+            <section className="product__other">
               {bakeryProducts?.map((item) => (
                 <CardItem key={item.id} product={item} />
               ))}
