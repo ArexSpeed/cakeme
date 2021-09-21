11c04b6e222c07c8f101e225fdadc39d6f56c8ee
Arek Cichocki <arexspeed95@gmail.com>
2021-09-21 10:40:48 +0200

    fix build problem, starsicon

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,11 +1,11 @@
 import Layout from 'components/Layout';
-//import ProductSite from 'components/ProductSite';
 import { getProduct, getProducts, getBakeryProducts } from 'services/products/getProduct';
 import { useEffect, useState } from 'react';
 import CardItem from 'components/CardItem';
 import { Favorite } from '@material-ui/icons';
 import { useSession } from 'next-auth/client';
 import AddToCartButton from 'components/AddToCartButton';
+import Stars from 'components/Stars';
 
 export const getStaticPaths = async () => {
   const products = await getProducts();
@@ -101,23 +101,6 @@ export default function ProductPage({ product, bakeryProducts }) {
     //window.location.reload(true);
   };
 
-  const starQty = () => {
-    let stars = [];
-    for (let i = 0; i < product.score; i++) {
-      stars.push(i);
-    }
-    return stars.map((star) => (
-      <svg
-        key={star}
-        className="product__rank-star"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg">
-        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-      </svg>
-    ));
-  };
-
   return (
     <Layout>
       <main className="main">
@@ -154,8 +137,8 @@ export default function ProductPage({ product, bakeryProducts }) {
             <div className="product__info">
               <div className="product__price">€{product?.price},00</div>
               <div className="product__rank">
-                <span className="product__rank-score">{product.score.toFixed(2)}</span>
-                {starQty()}
+                <span className="product__rank-score">{product?.score.toFixed(2)}</span>
+                <Stars qty={product?.score} />
               </div>
               <div className="product__description">{product?.description}</div>
               <div>
